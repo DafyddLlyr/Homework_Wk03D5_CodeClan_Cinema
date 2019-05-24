7793d9151e60b4e94d6ae641bb31db864c3c293a
dafyddllyr <dafyddllyr@gmail.com>
2019-05-24 17:40:02 +0100

    DB schema complete

---
+++ db/cinema.sql
@@ -0,0 +1,29 @@
+DROP TABLE IF EXISTS screenings;
+DROP TABLE IF EXISTS tickets;
+DROP TABLE IF EXISTS films;
+DROP TABLE IF EXISTS customers;
+
+CREATE TABLE customers (
+  id SERIAL8 PRIMARY KEY,
+  name VARCHAR(255),
+  funds INT8
+);
+
+CREATE TABLE films (
+  id SERIAL8 PRIMARY KEY,
+  title VARCHAR(255),
+  price INT8
+);
+
+CREATE TABLE tickets (
+  id SERIAL8 PRIMARY KEY,
+  customer_id INT8 REFERENCES customers(id) ON DELETE CASCADE,
+  film_id INT8 REFERENCES films(id) ON DELETE CASCADE
+);
+
+CREATE TABLE screenings (
+  id SERIAL8 PRIMARY KEY,
+  film_id INT8 REFERENCES films(id) ON DELETE CASCADE,
+  screening_time TIME,
+  number_of_tickets INT8
+);
