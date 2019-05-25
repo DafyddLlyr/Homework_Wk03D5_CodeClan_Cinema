cb9b5b8e9b6fd2446fb3efa4ba0e07dd099568fd
dafyddllyr <dafyddllyr@gmail.com>
2019-05-25 11:58:00 +0100

    Basic CRUD complete on Film class

--- console.rb
+++ console.rb
@@ -1,15 +1,21 @@
-require_relative("sql_runner")
-require_relative("../models/ticket")
-require_relative("../models/customer")
+require_relative("db/sql_runner")
+require_relative("models/ticket")
+require_relative("models/customer")
+require_relative("models/film")
 require("pry")
 
 Customer.delete_all()
+Film.delete_all()
 
 customer_1 = Customer.new({"name" => "Dafydd", "funds" => 100})
 customer_1.save()
 
 customer_2 = Customer.new({"name" => "Sion", "funds" => 200})
 customer_2.save()
+
+film_1 = Film.new({"title" => "Casino Royale", "price" => 5})
+film_1.save()
+
 
 binding.pry
 nil
